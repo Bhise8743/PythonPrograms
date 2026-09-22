f78d3186caa60425cbc8aca203632a806655cc5a
Bhise Omakr Balaji <omkarbhise8743@gmail.com>
2026-09-22 10:23:52 +0530

    Test case file of odd even program

---
+++ Test_Code/Test_Odd_Even.py
@@ -0,0 +1,11 @@
+from Code.Odd_Even import Even_Odd
+
+assert Even_Odd(23) == "Odd"
+print("Test case 1 pass ")
+
+assert Even_Odd(23) == "Odd"
+print("Test case 1 pass ")
+assert Even_Odd(23) == "Odd"
+print("Test case 1 pass ")
+assert Even_Odd(23) == "Odd"
+print("Test case 1 pass ")
