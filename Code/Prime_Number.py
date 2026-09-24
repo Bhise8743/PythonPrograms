e420b6e06dc21b71e7d61a711049393dc7313a38
Bhise Omakr Balaji <omkarbhise8743@gmail.com>
2026-09-24 14:29:30 +0530

    Prime Number Programs

--- Code/Prime_Number.py
+++ Code/Prime_Number.py
@@ -1,15 +1,20 @@
-def is_Prime(n):
-    if n < 2:
+def is_prime(number):
+    if number < 2:
         return False
-    i = 2
-    while i*i < n:
-        if n % i == 0:
+
+    for i in range(2, int(number ** 0.5) + 1):
+        if number % i == 0:
             return False
-        i += 1
-        
+
     return True
 
-print(is_Prime(17))
-print(is_Prime(14)) 
-   
+
+if __name__ == "__main__":
+    number = int(input("Enter a number: "))
+
+    if is_prime(number):
+        print("Prime")
+    else:
+        print("Not Prime")
         
+# python -m Code.Prime_Number
