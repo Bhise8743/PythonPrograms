1489ef80cab310f1527d854e193f6f55de056986
Bhise Omakr Balaji <omkarbhise8743@gmail.com>
2026-09-26 19:25:37 +0530

    Check Anagram String

---
+++ Code/Check_Anagram_Strings.py
@@ -0,0 +1,17 @@
+def are_anagrams(string1, string2):
+    string1 = string1.replace(" ", "").lower()
+    string2 = string2.replace(" ", "").lower()
+
+    return sorted(string1) == sorted(string2)
+
+
+if __name__ == "__main__":
+    string1 = input("Enter first string: ")
+    string2 = input("Enter second string: ")
+
+    if are_anagrams(string1, string2):
+        print("Anagrams")
+    else:
+        print("Not Anagrams")
+        
+# python -m pytest .\Test_Code\Test_Check_Anagram_Strings.py
