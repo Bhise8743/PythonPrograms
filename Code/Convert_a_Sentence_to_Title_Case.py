6a0565b2970aa18d8492e548d50ed2d537c96041
Bhise Omakr Balaji <omkarbhise8743@gmail.com>
2026-09-26 20:02:20 +0530

    Convert a Sentence to Title case

---
+++ Code/Convert_a_Sentence_to_Title_Case.py
@@ -0,0 +1,10 @@
+def title_case(sentence):
+    return sentence.title()
+
+
+if __name__ == "__main__":
+    sentence = input("Enter a sentence: ")
+
+    print(title_case(sentence))
+    
+# python .\Code\Convert_a_Sentence_to_Title_Case.py
